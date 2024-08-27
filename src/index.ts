7391af960d0954037dc08060f16f716c19fcc932
light <odinzero@outlook.com>
2024-08-27 08:50:43 +0800

    部署

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as useOutlet } from "./hook/useOutlet";
+export { default as Route } from "./components/Route";
+export { default as Routes } from "./components/Routes";
+export { default as Link } from "./components/Link";
+export { BrowserRouter, HashRouter, MemoryRouter } from "./components/router";
